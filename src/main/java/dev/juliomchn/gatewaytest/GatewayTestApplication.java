f2ee0a130c45610df3393443a582aceb9245035d
Julio Machin <litonfire1@gmail.com>
2024-01-03 17:06:16 +0100

    First commit

---
+++ src/main/java/dev/juliomchn/gatewaytest/GatewayTestApplication.java
@@ -0,0 +1,13 @@
+package dev.juliomchn.gatewaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayTestApplication.class, args);
+	}
+
+}
