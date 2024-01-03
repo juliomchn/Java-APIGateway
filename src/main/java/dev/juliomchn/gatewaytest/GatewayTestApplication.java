c8fbda14317884788961a2edf991636ede66fd79
Julio Machin <litonfire1@gmail.com>
2024-01-03 18:01:12 +0100

    Created a bean with a routing example

--- src/main/java/dev/juliomchn/gatewaytest/GatewayTestApplication.java
+++ src/main/java/dev/juliomchn/gatewaytest/GatewayTestApplication.java
@@ -2,6 +2,9 @@ package dev.juliomchn.gatewaytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class GatewayTestApplication {
@@ -9,5 +12,26 @@ public class GatewayTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayTestApplication.class, args);
 	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/posts/**")
+						.filters(f -> f
+							.prefixPath("/api")
+							.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+						)
+						.uri("http://localhost:8081")
+				)
+				.route(r -> r.path("/comments/**")
+						.filters(f -> f
+								.prefixPath("/api")
+								.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+						)
+						.uri("http://localhost:8082")
+				)
+				.build();
+	}
+//	
 
 }
